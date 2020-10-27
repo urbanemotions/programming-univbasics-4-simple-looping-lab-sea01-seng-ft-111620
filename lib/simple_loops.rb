c9d1cf43345ceed4e25254e2e3eed503ad7df8b9
Sin Wildy <sw.wildy@gmail.com>
2020-10-27 18:10:59 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -23,3 +23,12 @@ def output_array(array)
     counter += 1 
   end
 end
+
+
+def return_string_array(array)
+  counter = 0 
+  while counter < array.length do 
+    array[counter].to_s
+    counter += 1 
+  end
+end
