ebc978d1469e07ba13914c09a15633a446184c62
Sin Wildy <sw.wildy@gmail.com>
2020-10-27 18:04:58 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,3 +14,12 @@ def loop_message_n_times(message, number)
     counter += 1 
   end
 end
+
+
+def output_array(array)
+  counter = 0 
+  while counter < array.lenght do 
+    puts array[counter]
+    counter += 1 
+  end
+end
