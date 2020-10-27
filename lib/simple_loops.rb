938e7ad3bd5719364d2408a3c02623aa7f4f238d
Sin Wildy <sw.wildy@gmail.com>
2020-10-27 17:46:50 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,4 +1,8 @@
 def loop_message_five_times(message)
-  message = "Hello World"
-  
+  message = "Hello World."
+  counter = 0 
+  while message[counter] do
+    puts message[counter]
+    counter += 1 
+  end
 end
